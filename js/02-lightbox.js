67bf83b6ff0527bb51160ac73034e8baa6538c6c
LeonidLukin <leonidlukin90@gmail.com>
2022-08-16 17:40:47 +0300

    Task №2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import {
+    galleryItems
+} from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const itemsMarkup = createGalleryItem(galleryItems)
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup)
+gallery.addEventListener('click', onGalleryClick)
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({
+        preview,
+        original,
+        description
+    }) => {
+        return `
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                title="${description}"
+            />
+            </a>
+        `;
+    }).join('')
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+function onGalleryClick(e) {
+    e.preventDefault()
+
+    const isGalleryImage = e.target.classList.contains('gallery__image')
+
+    if (!isGalleryImage) {
+        return
+    }
+}
